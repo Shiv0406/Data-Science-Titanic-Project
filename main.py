9bb6aed8dc653d657af1368f7249a9e960eca1f6
Shiv0406 <66404236+Shiv0406@users.noreply.github.com>
2026-01-30 12:32:54 -0800

    Update main.py
    Day 2

--- main.py
+++ main.py
@@ -5,8 +5,18 @@
 
 import numpy as np
 import pandas as pd
-import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 
+# Datasets
+titanic = pd.read_csv('/Users/shivamshsthavara/Documents/projects/titanic project/train.csv')
+test = pd.read_csv('/Users/shivamshsthavara/Documents/projects/titanic project/test.csv')
+
+# exploring the data
+print(titanic.head())
+print(titanic.isnull().sum())
+print(titanic.info())
+
+#found cabin and age to be high in missing values with embarked only having 2 missing values
 
